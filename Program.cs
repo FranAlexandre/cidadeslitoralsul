00ed8a3154308c8f35cc9ca2b027011567e84391
franciny arielly <francinyalexandre07@gmail.com>
2021-09-18 10:52:08 -0300

    resolução exercicio cidadesdolitoralsul

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace cidadeslitoralsul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("praia grande");
+            Console.WriteLine("itanhaem");
+            Console.WriteLine("santos");
+            Console.WriteLine("momgagua");
+            Console.WriteLine("são vicente");
+
+
+
+
+        }
+    }
+}
